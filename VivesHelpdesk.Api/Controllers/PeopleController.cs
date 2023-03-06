ad01455c18073bee0ffa6c83c5fa44fc0d07c9b1
Ferretkint <92437473+Ferretkint@users.noreply.github.com>
2023-03-06 15:49:27 +0100

    Initial commit

---
+++ VivesHelpdesk.Api/Controllers/PeopleController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VivesHelpdesk.Model;
+using VivesHelpdesk.Services;
+
+namespace VivesHelpdesk.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PersonService _personService;
+
+        public PeopleController(PersonService personService)
+        {
+            _personService = personService;
+        }
+
+        //Find: /api/people
+        [HttpGet]
+        public IActionResult Find()
+        {
+            var people = _personService.Find();
+            return Ok(people);
+        }
+
+        //Get: /api/people/1
+        [HttpGet("{id:int}")]
+        public IActionResult Get([FromRoute]int id)
+        {
+            var people = _personService.Get(id);
+            return Ok(people);
+        }
+
+        //Create
+        [HttpPost]
+        public IActionResult Create([FromBody]Person person)
+        {
+            var createdPerson = _personService.Create(person);
+            return Ok(createdPerson);
+        }
+
+        //Edit
+        //Delete
+    }
+}
